0d2fa6a7cbc550a19e39f33db641962a0b7006b3
Gaurav Bhardwaj <gauravbharadwaj233@gmail.com>
2023-06-25 08:40:12 +0530

    first commit

---
+++ project1/app1/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class coffee(models.Model):
+    name=models.CharField(max_length=100)
+    amount=models.CharField(max_length=100)
+    payment_id=models.CharField(max_length=100)
+    paid=models.BooleanField(default=False)
